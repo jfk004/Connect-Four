d7f2bffd640b5cfca12af165c8ce4eaf7f6f64f7
Fahim <fahim@Fahims-MacBook-Pro-5.local>
2025-03-02 22:13:52 -0500

    Correction to Question 4

--- java/c4/ConnectFourAIPlayer.java
+++ java/c4/ConnectFourAIPlayer.java
@@ -6,13 +6,11 @@ import java.util.Random;
 import c4.mvc.ConnectFourModel;
 import c4.players.ConnectFourPlayer;
 
-
 public class ConnectFourAIPlayer extends ConnectFourPlayer {
 
     ConnectFourModel model;
     Random random;
 
-    
     public ConnectFourAIPlayer(ConnectFourModel model) {
         this.model = model;
         this.random = new Random();
@@ -29,16 +27,16 @@ public class ConnectFourAIPlayer extends ConnectFourPlayer {
 
     // Checks if the player has won or if the game has reached a draw
     public boolean terminalTest(int[][] board) {
-        return this.model.checkForWinner() != -1 || this.model.checkForDraw();
+        return this.model.checkForWinner() != ConnectFourModel.EMPTY || this.model.checkForDraw();
     }
 
-    public int[] actions(int [][] state){
-		ArrayList<Integer> moves = new ArrayList<Integer>();
-        
-        int row = 0; // Only checks the columns 
+    public int[] actions(int[][] state) {
+        ArrayList<Integer> moves = new ArrayList<Integer>();
 
-        for(int col= 0; col<6; col++){
-            if(state[row][col] == -1){
+        int row = 0; // Only checks the columns
+
+        for (int col = 0; col < 6; col++) {
+            if (state[row][col] == ConnectFourModel.EMPTY) {
                 moves.add(col);
             }
         }
@@ -48,29 +46,26 @@ public class ConnectFourAIPlayer extends ConnectFourPlayer {
         for (int i = 0; i < moves.size(); i++) {
             results[i] = moves.get(i);
         }
-		
-		return results;
-	}
 
+        return results;
+    }
 
-    public int[][] results(int[][] state, int action){
+    public int[][] results(int[][] state, int action) {
 
         int[][] newState = new int[state.length][state[0].length];
-        //deep copy of the board
-        for(int row = 0; row < state.length; row++){
-            for(int col = 0; col < state[0].length; col++){
+        // deep copy of the board
+        for (int row = 0; row < state.length; row++) {
+            for (int col = 0; col < state[0].length; col++) {
                 newState[row][col] = state[row][col];
             }
-
         }
 
-        // determine whose turn it is 
+        // determine whose turn it is
         int player = getTurn(state);
 
-
         // Find the lowest available row in the selected column
-        for(int row = state.length - 1; row>= 0; row--){
-            if(newState[row][action] == 0){    // empty spot found
+        for (int row = state.length - 1; row >= 0; row--) {
+            if (newState[row][action] == ConnectFourModel.EMPTY) { // empty spot found
                 newState[row][action] = player;
 
                 break;
@@ -78,28 +73,23 @@ public class ConnectFourAIPlayer extends ConnectFourPlayer {
         }
 
         return newState;
-     
+
     }
 
-    protected int getTurn(int[][] state){
+    protected int getTurn(int[][] state) {
         int empties = 0;
 
         // count empty spaces on the board
-        for(int row = 0; row < state.length; row++){
-            for(int col = 0; col < state[0].length; col++){
-                if(state[row][col] == 0){
+        for (int row = 0; row < state.length; row++) {
+            for (int col = 0; col < state[0].length; col++) {
+                if (state[row][col] == ConnectFourModel.EMPTY) {
                     empties++;
                 }
             }
         }
 
         // if empty count is odd, player 1 moves else player 2 moves
-        return(empties % 2 == 1) ? 1 : 2;
+        return (empties % 2 == 1) ? 2 : 1;
     }
 
 }
-
-
-
-
-
